8d32fa9e1bc6e3d01da1c3a9da9a3c229434cae0
Maitane Farinas Argoitia <maitane.farinas@bsc.es>
2026-06-30 18:30:37 +0200

    Translate python script

--- CreateCards.py
+++ CreateCards.py
@@ -1,6 +1,6 @@
-############################################
-# Bingo musikatuko kartoiak sortzeko kodea.
-############################################
+###########################################
+# Code to generate cards for musical bingo.
+###########################################
 import source.musical_bingo_lib as bm
 import argparse
 from pathlib import Path
@@ -9,18 +9,18 @@ from pathlib import Path
 def get_parser() -> argparse.ArgumentParser:
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "--abesti-zerrenda", required=True, type=Path,
-                        help="Abestien zerrendaren txt fitxategia. Adibide bat behar izanez gero begiratu fitxategiak/ karpetan.")
-    parser.add_argument("-o", "--latex-kodea", required=False, default="latex_kodea.txt",
-                        help="Latexeko kodea idazteko sortuko den fitxategiaren izena.")
-    parser.add_argument("-N", "--kartoi-kopurua", required=True, type=int,
-                        help="Nahi dugun kartoi kopurua, partidako. Partida bat baino gehiago izanez gero, bietan kopuru bera erabiliko da.")
-    parser.add_argument("-n", "--abesti-kopurua", required=True, type=int,
-                        help="Kartoi bakoitzeko abesti kopurua. 3ren multiploa!!!!!!")
-    parser.add_argument("-k", "--koloretan", action="store_true",
-                        help="Kartoiak koloretan inprimatzeko. Abesti bakoitzari ausazko kolore bat ematen zaio.")
-    parser.add_argument("-i", "--izenburua", type=str, required=False, default=None,
-                        help="Kartoiaren gainean idatziko den izenburua. Adib: 'BINGO MUSIKATUA'")
+    parser.add_argument("-a", "--file-list", required=True, type=Path,
+                        help="Txt file of the list of songs. Find an example in `files/` directory.")
+    parser.add_argument("-o", "--latex-code", required=False, default="whole_latex_code.txt",
+                        help="File that will be generated with the LaTeX code.")
+    parser.add_argument("-N", "--number-of-cards", required=True, type=int,
+                        help="Number of bingo cards for the game. If more than a game, the same number will be used in both.")
+    parser.add_argument("-n", "--number-of-songs", required=True, type=int,
+                        help="Number of songs per card. Multiple of 3!!!!!!")
+    parser.add_argument("-k", "--colour", action="store_true",
+                        help="Add this flag to generate cards in colour.")
+    parser.add_argument("-i", "--title", type=str, required=False, default=None,
+                        help="Title that will be added on top of the cards. Adib: 'MUSICAL BINGO'")
 
     return parser
 
@@ -34,34 +34,34 @@ def get_command_line_arguments():
 def main():
     args = get_command_line_arguments()
 
-    zerrenda_fitx = args.abesti_zerrenda.as_posix()
-    kartoi_kopurua = args.kartoi_kopurua
-    output_fitxategia = args.latex_kodea
-    abesti_kopurua = args.abesti_kopurua
-    koloretan = args.koloretan
-    izenburua = args.izenburua
+    song_file = args.file_list.as_posix()
+    number_cards = args.numer_of_cards
+    output_file = args.latex_code
+    number_songs = args.number_of_songs
+    in_colour = args.colour
+    title = args.title
 
-    if izenburua:
-        bm.kartoiak_idatzi_n(kartoiko_abesti_kopurua=abesti_kopurua,
-                kartoi_kopurua=kartoi_kopurua,
-                abesti_zerrenda_filename=zerrenda_fitx,  # Gure abestien zerrenda (hau adibide moduan dagoena da, partida bakarrarekin)
-                output_fitxategia_filename=output_fitxategia,  # Latex-eko kodearekin sortuko den fitxategia
-                koloretan=koloretan,   # Koloreak nahi baditugu, bestela ezabatu zati hau
-                izenburua=izenburua
+    if title:
+        bm.write_cards(songs_per_card=number_songs,
+                number_of_cards=number_cards,
+                song_filename=song_file,  # Gure abestien zerrenda (hau adibide moduan dagoena da, partida bakarrarekin)
+                output_filename=output_file,  # Latex-eko kodearekin sortuko den fitxategia
+                add_colour=in_colour,   # Koloreak nahi baditugu, bestela ezabatu zati hau
+                title=title
                 )
     else:
-        bm.kartoiak_idatzi_n(kartoiko_abesti_kopurua=abesti_kopurua,
-                        kartoi_kopurua=kartoi_kopurua,
-                        abesti_zerrenda_filename=zerrenda_fitx,  # Gure abestien zerrenda (hau adibide moduan dagoena da, partida bakarrarekin)
-                        output_fitxategia_filename=output_fitxategia,  # Latex-eko kodearekin sortuko den fitxategia
-                        koloretan=koloretan   # Koloreak nahi baditugu, bestela ezabatu zati hau
+        bm.write_cards(songs_per_card=number_songs,
+                        number_of_cards=number_cards,
+                        song_filename=song_file,  # Gure abestien zerrenda (hau adibide moduan dagoena da, partida bakarrarekin)
+                        output_filename=output_file,  # Latex-eko kodearekin sortuko den fitxategia
+                        add_colour=in_colour   # Koloreak nahi baditugu, bestela ezabatu zati hau
                         )
 
-    output_fitxategia = Path(output_fitxategia)
-    if output_fitxategia.is_file():
-        print(f"Ondo sortu da {output_fitxategia.as_posix()} fitxategia!")
+    output_file = Path(output_file)
+    if output_file.is_file():
+        print(f"File {output_file.as_posix()} generated correctly!")
     else:
-        print(f"Arazo bat egon da. Ezin izan da {output_fitxategia.as_posix()} fitxategia sortu.")
+        print(f"There was an issue. File {output_file.as_posix()} was not properly generated.")
 
 if __name__ == "__main__":
     main()
